f86364b622ce375b668a3063dff7d98e0bb6795e
Florent Damiens <damiens.florent@orange.fr>
2020-07-13 22:02:32 +0200

    style

--- js/app.js
+++ js/app.js
@@ -92,7 +92,7 @@
 		var giqa= new RadialGauge
 		(
 			'iqa',
-			{"units":" ","minValue":0,"maxValue":300,"majorTicks":["0","50","100","150","200", "250", "300+"],"colors":{"majorTicks":"#FFFFFF"},"strokeTicks":false,"highlights":[{"from":0,"to":50,"color":"#20A120"},{"from":50,"to":100,"color":"#AFCD0A"},{"from":100,"to":150,"color":"#F9BA0D"},{"from":150,"to":200,"color":"#FD6828"},{"from":200,"to":250,"color":"#BB0000"},{"from":250,"to":300,"color":"#853085"}]}
+			{"units":"", "valueFormat": {"dec": 0}, "minValue":0,"maxValue":300,"majorTicks":["0","50","100","150","200", "250", "300+"],"colors":{"majorTicks":"#FFFFFF"},"strokeTicks":false,"highlights":[{"from":0,"to":50,"color":"#20A120"},{"from":50,"to":100,"color":"#AFCD0A"},{"from":100,"to":150,"color":"#F9BA0D"},{"from":150,"to":200,"color":"#FD6828"},{"from":200,"to":250,"color":"#BB0000"},{"from":250,"to":300,"color":"#853085"}]}
 		);
 		giqa.draw();
 		giqa.setValue(45);
